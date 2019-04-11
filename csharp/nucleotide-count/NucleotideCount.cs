a665cdc545dd0424820ab82c6f64f060866d2860
agpaoak <agpaoak@gmail.com>
2019-04-10 17:56:01 -0700

    Added nucleotide solution

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,26 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IDictionary<char, int> result = new Dictionary<char, int>
+        {
+            ['A'] = 0,
+            ['C'] = 0,
+            ['G'] = 0,
+            ['T'] = 0
+        };
+
+        foreach(var dna in sequence.ToCharArray())
+        {
+            if (result.ContainsKey(dna))
+            {
+                result[dna] += 1;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        return result;
     }
 }
