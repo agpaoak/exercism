b3c3cc23f919828e7293e6d18f9b3d113ff3dad5
agpaoak <agpaoak@gmail.com>
2019-06-02 19:18:20 -0700

    Third unit test pass

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,19 +1,50 @@
 using System;
+using System.Text;
 
 public class Robot
 {
 
     private string _name;
+
+
+    public Robot()
+    {
+        _name = this.CreateName();
+    }
+
     public string Name
     {
         get
         {
-            return _name = "AA001";
+            return _name;
         }
     }
 
     public void Reset()
     {
         _name = string.Empty;
+    }
+
+    private string CreateName()
+    {
+        var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        const int MAXCHAR = 2;
+        const int MAXNUM = 3;
+        StringBuilder sb = new StringBuilder();
+
+
+        for(var i = 0; i < MAXCHAR; i++)
+        {
+            Random rnd = new Random();
+            sb.Append(charset[rnd.Next(charset.Length)]);
+        }
+
+        for(var i = 0; i < MAXNUM; i++)
+        {
+            Random rnd = new Random();
+            sb.Append(rnd.Next(0,9));
+        }
+
+        return this._name = sb.ToString();
     }
 }
