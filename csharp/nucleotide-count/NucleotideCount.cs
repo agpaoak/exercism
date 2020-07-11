6e91c2b9e68bfa82c2b6225df8e0fef3d6760892
Kevin Agpaoa <agpaoak@users.noreply.github.com>
2020-07-11 11:30:45 -0700

    Py rainrops (#8)
    * Added acronym file
    
    * Added another exercise
    
    * Passed first test
    
    * Passed all tests
    
    * Two tests passed
    
    * Starting new exercise
    
    * space-age exercise
    
    * Updated using lambda expression
    
    * Resistor Color exercise added
    
    * High Scores
    
    * Updated pangram
    
    * All tests pass
    
    * Added Hamming exercise
    
    * Hamming all tests completed
    
    * Hamming
    
    * Added nucleotide solution
    
    * Added nucleotide, all tests pass
    
    * nucleotide
    
    * Added Robot Names
    
    * First unit test pass
    
    * Second unit test passes
    
    * Third unit test pass
    
    * Fourth test pass
    
    * Fifth test passes
    
    * Tests pass
    
    * Save updates

---
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        //
+        Regex unacceptedChars = new Regex("[^ACGT]");
+
+        if (unacceptedChars.IsMatch(sequence))
+        {
+            throw new ArgumentException();
+        }
+        else
+        {
+            return new Dictionary<char, int>
+            {
+                ['A'] = sequence.Count(a => a == 'A'),
+                ['C'] = sequence.Count(c => c == 'C'),
+                ['G'] = sequence.Count(g => g == 'G'),
+                ['T'] = sequence.Count(t => t == 'T')
+            };
+        }
+    }
+}
