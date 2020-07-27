b92b1b482d252cf2d0d634ee40b7aa1379e502c8
agpaoak <agpaoak@gmail.com>
2020-07-26 21:52:58 -0700

    Updated with new sorting

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,24 +1,30 @@
 def latest(scores):
-    length = len(scores)
-    return scores[length-1]
+    return scores[len(scores)-1]
 
 def personal_best(scores):
-    cp_scores = scores.copy()
-    cp_scores.sort()
-    return latest(cp_scores)
+    return latest(sort_asc(scores))
 
 def personal_top_three(scores):
-
-    cp_scores = scores.copy()
+    cp_scores = sort_desc(scores)
 
     if (len(scores) == 1):
         return cp_scores
     else:
         top_three = []
-        cp_scores.sort()
-        cp_scores.reverse()
 
-        while(len(top_three) != 3) and (len(cp_scores) != 0 ):
+        while   (len(top_three) != 3) and (
+                len(cp_scores) != 0 ):
+                
             top_three.append(cp_scores.pop(0))
 
         return top_three
+
+def sort_desc(scores):
+    cp_scores = sort_asc(scores)
+    cp_scores.reverse()
+    return cp_scores
+
+def sort_asc(scores):
+    cp_scores = scores.copy()
+    cp_scores.sort()
+    return cp_scores
