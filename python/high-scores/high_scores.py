baca2686dd340dd2991e4acf6c6578e5218699b3
agpaoak <agpaoak@gmail.com>
2020-07-27 09:22:05 -0700

    Updated with tips from mentor

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,30 +1,10 @@
 def latest(scores):
-    return scores[len(scores)-1]
+    return scores[-1]
 
 def personal_best(scores):
-    return latest(sort_asc(scores))
+    return max(scores)
 
 def personal_top_three(scores):
-    cp_scores = sort_desc(scores)
+    temp_scores = sorted(scores, reverse=True)
 
-    if (len(scores) == 1):
-        return cp_scores
-    else:
-        top_three = []
-
-        while   (len(top_three) != 3) and (
-                len(cp_scores) != 0 ):
-                
-            top_three.append(cp_scores.pop(0))
-
-        return top_three
-
-def sort_desc(scores):
-    cp_scores = sort_asc(scores)
-    cp_scores.reverse()
-    return cp_scores
-
-def sort_asc(scores):
-    cp_scores = scores.copy()
-    cp_scores.sort()
-    return cp_scores
+    return temp_scores[0:3]
