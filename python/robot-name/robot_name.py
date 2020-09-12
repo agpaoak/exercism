b3a287b83e6fef361b555846ec2d3e12593cdee5
agpaoak <agpaoak@gmail.com>
2020-09-12 00:22:30 -0700

    Passing

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -9,22 +9,24 @@ class Robot:
         self.reset()
 
     def reset(self):
-        self.name = self.__get_name()
-        self.names.append(self.name)
-
+        self.name = self.get_name()
+        Robot.names.append(self.name)
+    
     def __get_rand_int(self):
         return str(random.randrange(0,999)).zfill(3)
 
     def __get_rand_char(self):
         letters ="abcdefghijklmnopqrstuvwxyz"
-
         return random.choice(letters.upper())
 
-    def __get_name(self):
-        temp_name = "{}{}{}".format(self.__get_rand_char(),self.__get_rand_char(), self.__get_rand_int())
+    def __create_name(self):
+        return "{}{}{}".format(self.__get_rand_char(), self.__get_rand_char(), self.__get_rand_int())
 
-        while temp_name not in self.names:
-            temp_name = "{}{}{}".format(self.__get_rand_char(),self.__get_rand_char(), self.__get_rand_int())
-        self.names.append(temp_name)
+    def get_name(self):
+     
+        temp_name = self.__create_name()
+
+        while temp_name in Robot.names:
+            temp_name = self.__create_name()
 
         return temp_name
