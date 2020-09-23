06ce72e0871dc232b1ab9bef47fcbf63b612dd20
agpaoak <agpaoak@gmail.com>
2020-09-22 20:32:50 -0700

    Refactored Robot class

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -12,26 +12,17 @@ class Robot:
     def reset(self):
         self.name = self.get_name()
         Robot.names.append(self.name)
-    
-    def __get_rand_int(self):
-        max_num = 999
-        return str(secrets.randbelow(max_num + 1)).zfill(3)
-
-    def __get_rand_char(self):
-        letters = string.ascii_uppercase
-        return secrets.choice(letters)
 
     def __create_name(self):
-        return f'{self.__get_rand_char()}{self.__get_rand_char()}{self.__get_rand_int()}'
+        letters = string.ascii_uppercase
+        name = [secrets.choice(letters) for i in range(2)]
+        name.append(str(secrets.randbelow(1000)).zfill(3))
+        return ''.join(name)
 
     def get_name(self):
-     
         temp_name = self.__create_name()
 
         while temp_name in Robot.names:
             temp_name = self.__create_name()
 
         return temp_name
-
-
-        string.asc
