8adbd6e6d2ef9ca4bb1b01409477fa764dc8c172
agpaoak <agpaoak@gmail.com>
2020-09-22 17:59:30 -0700

    Refactored class

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,18 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.ms = matrix_string.split('\n')
+        self.matrix = [arr.split(' ') for arr in matrix_string.split('\n')]
 
     def row(self, index):
-        return self.__convert_to_int(self.ms[index - 1].split())
-
+        return [int(row) for row in self.matrix[index-1]]
+    
     def column(self, index):
-
-        c = []
-
-        for row in self.ms:
-            c.append(row.split()[index - 1])
-
-        return self.__convert_to_int(c)
-
-    def __convert_to_int(self, arr):
-        return list(map(lambda x: int(x), arr))
+        return [int(row[index-1]) for row in self.matrix]
