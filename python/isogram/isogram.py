166137389eda701aa4f34ec192053705a78760a8
Kevin Agpaoa <agpaoak@users.noreply.github.com>
2020-09-26 08:54:28 -0700

    Py isogram (#18)
    * Added isogram exercise
    
    * Passed all tests

---
+++ python/isogram/isogram.py
@@ -0,0 +1,9 @@
+import re
+
+def is_isogram(string):
+    normalize = re.sub('\W', '',string.lower())
+    
+    for c in normalize:
+        if normalize.count(c) > 1:
+            return False
+    return True
