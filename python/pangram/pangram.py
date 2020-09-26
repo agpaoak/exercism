b001d4f72249f006766e2d6dcace52cc420980b6
Kevin Agpaoa <agpaoak@users.noreply.github.com>
2020-09-26 09:24:57 -0700

    Py pangram (#19)
    * Added isogram exercise
    
    * Passed all tests
    
    * Pangram tests passed

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,14 @@
+import re
+import string
+
 def is_pangram(sentence):
-    pass
+    
+    normalize = re.sub('[^a-z]', '', sentence.lower())
+
+    letters = []
+    for c in normalize:
+        if c not in letters:
+            letters.append(c)
+            
+    letters.sort()
+    return ''.join(letters) == string.ascii_lowercase
