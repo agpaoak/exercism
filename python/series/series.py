6bf4e1a19380ff671dec7c997d38848e1dbe7476
Kevin Agpaoa <agpaoak@gmail.com>
2020-09-27 17:43:18 -0700

    Added Series exercise, tests also pass

---
+++ python/series/series.py
@@ -0,0 +1,16 @@
+def slices(series, length):
+    if length <= 0:
+        raise ValueError('Length cannot be 0 or less.')
+    elif not series:
+        raise ValueError('Series cannot be empty')
+    elif length > len(series):
+        raise ValueError('Length cannot be greater than the length of Series')
+    elif len(series) >= length:        
+        sliced = []
+        for n in range(len(series)):
+            value = series[n:n+length]
+            if len(value) == length:
+                sliced.append(value)
+            else:
+                break   
+        return sliced
