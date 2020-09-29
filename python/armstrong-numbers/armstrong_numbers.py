0c974ea968f1e5a4de16a723cae680a8ffd68f02
Kevin Agpaoa <agpaoak@gmail.com>
2020-09-29 05:48:47 -0700

    Applied codestyle

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,8 +1,9 @@
 import math
 
+
 def is_armstrong_number(number):
     num_list = list(map(int, str(number)))
     power = len(num_list)
-    powers = [ int(math.pow(base, power)) for base in num_list ]
- 
+    powers = [int(math.pow(base, power)) for base in num_list]
+
     return number == sum(powers)
