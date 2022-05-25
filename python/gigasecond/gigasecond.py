03b26583f5ee68a0ec991e533493695495402d7b
Kevin Agpaoa <agpaoak@users.noreply.github.com>
2022-05-24 18:31:10 -0700

    All tests pass

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,9 @@
+import math
+import datetime
+
+# A gigasecond is 10^9 (1,000,000,000) seconds.
+# moment = class datetime
 def add(moment):
-    pass
+    GIGASECOND = math.pow(10, 9)
+
+    return moment + datetime.timedelta(seconds=GIGASECOND)
